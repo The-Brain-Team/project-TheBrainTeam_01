7d3ded7d54683f32b0ffa1ea17105096dfd5b240
hilal_dinc <hilaldincc94@gmail.com>
2025-04-20 17:19:24 +0300

    reviews

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// reviews-drag.js
+const slider = document.querySelector('.reviews-list');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('dragging');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('dragging');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('dragging');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.2; // 💡 Yumuşak kayma hızı
+  slider.scrollLeft = scrollLeft - walk;
+});
