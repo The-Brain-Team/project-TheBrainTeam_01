17bd252908c4ae229350f1fe41d9060ad73b5cb4
Saim Durmuşoğlu <saim.durmusoglu@gmail.com>
2025-04-20 16:34:26 +0300

    20.04.2025 16:34 - temizleme ve saim brunch güncelleme

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-const menuOpen = document.getElementById('menu-open');
-
-const menu = document.getElementById('menu');
-
-const menuClose = document.getElementById('menu-close');
-
-menuOpen.addEventListener('click', function () {
-  menu.classList.add('is-open-menu');
-});
-
-menuClose.addEventListener('click', function () {
-  menu.classList.remove('is-open-menu');
-});
+// kodlar buaraya yazılacak...
