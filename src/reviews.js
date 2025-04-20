a5095cce6d4c9fe37c5930c79c160a397b884b92
hilal_dinc <hilaldincc94@gmail.com>
2025-04-21 01:19:52 +0300

    owl-reviews

--- src/reviews.js
+++ src/reviews.js
@@ -1,31 +1,21 @@
-// reviews-drag.js
-const slider = document.querySelector('.reviews-list');
+if (window.innerWidth < 1280) {
 
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('dragging');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+$(".owl-carousel").owlCarousel({
+  loop: false,
+  nav: true,
+  dots: false,
+  autoHeight: false,
+  stagePadding: 0,
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 2
+    },
+    1280: {
+      items: 3
+    }
+  }
 });
-
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('dragging');
-});
-
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('dragging');
-});
-
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 1.2; // 💡 Yumuşak kayma hızı
-  slider.scrollLeft = scrollLeft - walk;
-});
+  }
